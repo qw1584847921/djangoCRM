13378c80ea8f3a362dfd544457cf7008d42f2d8c
lei <1584847921@qq.com>
2017-10-13 18:52:17 +0800

    x

--- djangoCRM/settings.py
+++ djangoCRM/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crm',
+    'student',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'djangoCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PerfectCRM',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -118,4 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'statics'),
+)
--- student/views.py
+++ student/views.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-
-def customer_list(request):
-    return render(request, 'sales/customers.html')
-
+    return render(request, 'student/index.html')
